6b781dbffbbbf48f37092cc632c621827a18e5f8
root00198 <naman00198@gmail.com>
2020-01-14 19:28:36 +0530

    some cleanup

--- test/core/modulesGIFTest/index.js
+++ test/core/modulesGIFTest/index.js
@@ -1,46 +1,7 @@
-require('./add-qr.js');
-require('./average.js');
-require('./blur.js');
-require('./brightness.js');
-require('./canvas-resize.js');
-require('./channel.js');
-require('./color-temperature.js');
-require('./colormap.js');
-require('./contrast.js');
-require('./convolution.js');
-require('./crop.js');
-require('./dither.js');
-require('./draw-rectangle.js');
-require('./dynamic.js');
-require('./edge-detect.js');
-require('./exposure.js');
-require('./flip-image.js');
-require('./gamma-correction.js');
-require('./gradient.js');
-require('./grid-overlay.js');
-require('./invert.js');
-require('./ndvi-colormap.js');
-require('./ndvi.js');
-require('./noise-reduction.js');
-require('./replace-color.js');
-require('./resize.js');
-require('./rotate.js');
-require('./saturation.js');
-require('./threshold.js');
-require('./tint.js');
-require('./white-balance.js');
+const fs = require('fs');
+const files = fs.readdirSync('./test/core/modulesGIFTest/');
 
-// LEFT //
-/*
-* Blob Analysis //<<--
-* Colorbar //<<--
-* DecodeQR //<<--
-* FishEyeGl //<<--
-* Histogram //<<--
-* Import Image //<<--
-* Minify Image //<<--
-* Overlay //<<--
-* Paint Bucket //<<--
-* TextOverlay //<<--
-* Webgl Distort //<<--
-*/
+files.forEach((f)=>{
+  if(f != 'index.js')
+    require('./' + f);
+});
