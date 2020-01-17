a49b51c3d6b0d5c7307e129244c16ce02b785962
root00198 <naman00198@gmail.com>
2020-01-18 01:41:31 +0530

    resolve conflicts

--- test/core/modules/contrast.js
+++ test/core/modules/contrast.js
@@ -1,4 +1,10 @@
-const benchmark = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAklEQVR4AewaftIAAAAsSURBVKXBAQEAMAiAME5Dgz6jhmB7f2YJJJJIIokkkkgiiSSSSCKJJJJIogOcOQJuFks/jgAAAABJRU5ErkJggg==',
-  testModule = require('../templates/module-test');
+const benchmark = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAklEQVR4AewaftIAAAApSURBVKXBAQEAAAiDMKR/5xuC7QYjkEgiiSSSSCKJJJJIIokkkkgiiR5YbQIegx78CAAAAABJRU5ErkJggg==',
+  _benchmark = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAklEQVR4AewaftIAAAApSURBVKXBAQEAAAiDMKR/5xuC7QYjkEgiiSSSSCKJJJJIIokkkkgiiR5YbQIegx78CAAAAABJRU5ErkJggg==',
+  testModule = require('../templates/module-test'),
+  optionsChange = require('../templates/options-test'),
+  option = {contrast: 40},
+  _option = {contrast: 84};
 
-testModule('contrast', {contrast: -40}, benchmark);
+testModule('contrast', option, benchmark);
+
+optionsChange('contrast', [option, _option], [benchmark, _benchmark]);
